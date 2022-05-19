0b792043968c006deb1d25cb8d18013166e8a45c
harsha16208 <sriharsha16208@gmail.com>
2022-05-19 18:50:39 +0530

    first commit:

---
+++ src/main/java/com/harsha/app/App.java
@@ -0,0 +1,13 @@
+package com.harsha.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Jenkins app" );
+    }
+}
